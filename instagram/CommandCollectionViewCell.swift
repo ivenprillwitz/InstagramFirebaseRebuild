54d4cf5a37b6af67b2cfa12156b0b090cec1b5bb
Iven Prillwitz <iven@ivens-macbook-pro-2.local>
2017-05-21 11:40:37 +0200

    Added CommandCell

---
+++ instagram/CommandCollectionViewCell.swift
@@ -0,0 +1,104 @@
+//
+//  CommandCollectionViewCell.swift
+//  instagram
+//
+//  Created by Iven Prillwitz on 20.05.17.
+//  Copyright © 2017 Petch. All rights reserved.
+//
+
+import UIKit
+import FaveButton
+
+class CommandCollectionViewCell: UICollectionViewCell {
+    
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        
+        setupView()
+    }
+    required init(coder aDecoder:NSCoder) {
+        fatalError("init(coder:) has not been implemnted")
+    }
+    
+    fileprivate func setupView(){
+        
+        let buttonSize: CGFloat = 40
+        let buttonMargin: CGFloat = 6
+        
+        self.addSubview(heartButton)
+        
+        heartButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        heartButton.leftAnchor.constraint(equalTo: self.leftAnchor, constant: buttonMargin).isActive = true
+        heartButton.widthAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        heartButton.heightAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        
+        
+        self.addSubview(commentButton)
+        
+        commentButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        commentButton.leftAnchor.constraint(equalTo: heartButton.rightAnchor, constant: buttonMargin).isActive = true
+        commentButton.widthAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        commentButton.heightAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        
+        self.addSubview(shareButton)
+        
+        shareButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        shareButton.leftAnchor.constraint(equalTo: commentButton.rightAnchor, constant: buttonMargin).isActive = true
+        shareButton.widthAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        shareButton.heightAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        
+        self.addSubview(bookmarkButton)
+        
+        bookmarkButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        bookmarkButton.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        bookmarkButton.widthAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        bookmarkButton.heightAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        
+    }
+    
+    
+    let heartButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named:"heart"), for: .normal)
+        button.contentHorizontalAlignment = UIControlContentHorizontalAlignment.fill
+        button.contentVerticalAlignment = UIControlContentVerticalAlignment.fill
+        button.imageEdgeInsets = UIEdgeInsets(top: 8, left: 7, bottom: 8, right: 7)
+        button.tintColor = .black
+        return button
+    }()
+    
+    let commentButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named:"comment"), for: .normal)
+        button.contentHorizontalAlignment = UIControlContentHorizontalAlignment.fill
+        button.contentVerticalAlignment = UIControlContentVerticalAlignment.fill
+        button.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        button.tintColor = .black
+        return button
+    }()
+    
+    let shareButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named:"paperplane"), for: .normal)
+        button.tintColor = .black
+        button.contentHorizontalAlignment = UIControlContentHorizontalAlignment.fill
+        button.contentVerticalAlignment = UIControlContentVerticalAlignment.fill
+        button.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        return button
+    }()
+    
+    let bookmarkButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named:"bookmark"), for: .normal)
+        button.tintColor = .black
+        button.contentHorizontalAlignment = UIControlContentHorizontalAlignment.fill
+        button.contentVerticalAlignment = UIControlContentVerticalAlignment.fill
+        button.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        return button
+    }()
+}
--- instagram/PostSectionController.swift
+++ instagram/PostSectionController.swift
@@ -16,20 +16,38 @@ class PostSectionController: ListSectionController {
     
 
     override func numberOfItems() -> Int {
-        return 1
+        return 2
     }
     
     override func sizeForItem(at index: Int) -> CGSize {
-        return CGSize(width: collectionContext!.containerSize.width, height: 500)
+        
+        switch index {
+        case 0:
+            return CGSize(width: collectionContext!.containerSize.width, height: 500)
+        case 1:
+            return CGSize(width: collectionContext!.containerSize.width, height: 100)
+        default:
+            return CGSize(width: collectionContext!.containerSize.width, height: 500)
+        }
+        
     }
     
     
     override func cellForItem(at index: Int) -> UICollectionViewCell {
         
-        guard let cell = collectionContext?.dequeueReusableCell(of: FeedCollectionViewCell.self, for: self, at: index) as? FeedCollectionViewCell else {
-            fatalError()
+        if index == 0 {
+            guard let cell = collectionContext?.dequeueReusableCell(of: FeedCollectionViewCell.self, for: self, at: index) as? FeedCollectionViewCell else {
+                fatalError()
+            }
+            return cell
         }
-        return cell
+        if index == 1 {
+            guard let cell = collectionContext?.dequeueReusableCell(of: CommandCollectionViewCell.self, for: self, at: index) as? CommandCollectionViewCell else {
+                fatalError()
+            }
+            return cell
+        }
+        return UICollectionViewCell()
     }
     
     override func didUpdate(to object: Any) {
